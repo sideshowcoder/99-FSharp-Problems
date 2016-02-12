9252de2ae48286cbac4d5d1663b2336130bafd30
Philipp Fehre <philipp@fehre.co.uk>
2016-02-12 22:16:41 +0000

    Initial

---
+++ 99_fsharp_problems/Lists.fs
@@ -0,0 +1,23 @@
+﻿
+module MyList =
+    open System
+    open NUnit.Framework
+    open FsUnit
+
+    let rec myLast list = 
+        match list with
+        | [] -> raise(new ArgumentException("foo"))
+        | hd :: [] -> hd
+        | hd :: tl -> myLast tl
+
+    [<Test>]
+    let myLastTest() =
+        myLast [ 1; 2; 3; ] |> should equal 3
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Lists"
+    printfn "myLast [ 1; 2; 3 ] -> %i" (MyList.myLast [ 1; 2; 3; ])
+    System.Console.ReadLine()
+    0 // return an integer exit code
---
+++ 99_fsharp_problems/README.md
@@ -0,0 +1,13 @@
+﻿99 F# Problems
+==============
+
+Based on [99 Haskell Problems](https://wiki.haskell.org/99_Haskell_exercises), which in 
+turn are based on [99_Ninety-Nine_Lisp_Problem](http://www.ic.unicamp.br/~meidanis/courses/mc336/2006s2/funcional/L-99_Ninety-Nine_Lisp_Problems.html), 
+which is based [99 Prolog Problems](https://prof.ti.bfh.ch/hew1/informatik3/prolog/p-99/).
+
+
+
+
+
+
+
